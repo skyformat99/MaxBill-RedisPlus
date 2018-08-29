08968a6bafc6fe4cf1290f14bf71e41f12518468
MaxBill <1370581389@qq.com>
2018-08-29 11:23:05 +0800

    自定义窗体标题栏，复写系统默认监听

--- src/main/java/com/maxbill/core/desktop/DesktopApp.java
+++ src/main/java/com/maxbill/core/desktop/DesktopApp.java
@@ -1,7 +1,9 @@
 package com.maxbill.core.desktop;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -15,7 +17,7 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class DesktopApp extends Application {
-    private final String uiType = "0";
+    private final String uiType = "1";
     private final Integer prefWidth = 1300;
     private final Integer prefHeight = 800;
     private final Integer titlePrefWidth = 30;
@@ -55,6 +57,7 @@ public class DesktopApp extends Application {
         stage.setScene(new Scene(getWinRoot(stage)));
         stage.getIcons().add(new Image(appIcon));
         stage.setResizable(false);
+        stage.setAlwaysOnTop(true);
         stage.show();
     }
 
@@ -71,6 +74,9 @@ public class DesktopApp extends Application {
         VBox content = new VBox();
         content.getChildren().addAll(getWebView());
         winRoot.getChildren().addAll(getTopView(stage), content);
+        winRoot.setOnDragDone(event -> {
+            //TODO 拖拽逻辑
+        });
         return winRoot;
     }
 
@@ -92,7 +98,6 @@ public class DesktopApp extends Application {
         // 顶部视图
         VBox topView = new VBox();
         topView.setId("topView");
-        topView.setPrefSize(prefWidth, titlePrefHeight);
         // 标题栏视图
         AnchorPane titleView = new AnchorPane();
         // 标题图标
@@ -105,16 +110,19 @@ public class DesktopApp extends Application {
         winTitle.setPrefSize(100, titlePrefHeight);
         winTitle.setId("winTitle");
         winTitle.setText(appName);
+        winTitle.setAlignment(Pos.BASELINE_LEFT);
         titleView.getChildren().add(winTitle);
         // 最小化按钮
         Label winAbate = new Label();
         winAbate.setPrefSize(titlePrefWidth, titlePrefHeight);
         winAbate.setId("winAbate");
-        winAbate.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent event) {
-                if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 1) {
-                    stage.close();
-                }
+        winAbate.setText("一");
+        winAbate.setScaleX(0.6);
+        winAbate.setScaleY(0.6);
+        winAbate.setAlignment(Pos.BASELINE_CENTER);
+        winAbate.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 1) {
+                stage.setIconified(true);
             }
         });
         titleView.getChildren().add(winAbate);
@@ -122,22 +130,23 @@ public class DesktopApp extends Application {
         Label winClose = new Label();
         winClose.setPrefSize(titlePrefWidth, titlePrefHeight);
         winClose.setId("winClose");
-        winClose.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent event) {
-                if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 1) {
-                    stage.setIconified(true);
-                }
+        winClose.setText("X");
+        winClose.setScaleX(0.6);
+        winClose.setScaleY(0.6);
+        winClose.setAlignment(Pos.BASELINE_CENTER);
+        winClose.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 1) {
+                Platform.exit();
             }
         });
         titleView.getChildren().add(winClose);
         // 顶部布局
         AnchorPane.setLeftAnchor(winImage, 5.0);
         AnchorPane.setLeftAnchor(winTitle, 40.0);
-        AnchorPane.setRightAnchor(winAbate, 40.0);
+        AnchorPane.setRightAnchor(winAbate, 30.0);
         AnchorPane.setRightAnchor(winClose, 5.0);
         topView.getChildren().add(titleView);
         return topView;
     }
-
 
 }
